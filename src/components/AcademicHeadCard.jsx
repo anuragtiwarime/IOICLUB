e2ed432cec68dc7afb2023ce2e3211f5c5067056
Ankit Raj <ankitraj07032002@gmail.com>
2025-01-12 16:22:23 +0530

    academicHead section changed

--- src/components/AcademicHeadCard.jsx
+++ src/components/AcademicHeadCard.jsx
@@ -3,19 +3,21 @@ import { FaLinkedin } from 'react-icons/fa';
 
 export default function AcademicHeadCard({ image, name, role, description, linkedin }) {
   return (
-    <div className="bg-white rounded-lg shadow-lg p-6 transform transition-transform hover:scale-105 flex flex-row justify-between h-full gap-4">
-      <div className="flex flex-col items-center mb-4 w-1/2 ">
+    <div className="bg-white rounded-lg shadow-lg p-6 transform transition-transform hover:scale-105 flex flex-col md:flex-row md:justify-between h-full gap-8">
+      {/* Image Section */}
+      <div className="flex flex-col items-center mb-4 md:mb-0 md:w-1/2">
         <img
           src={image}
           alt={name}
           className="w-28 h-28 rounded-full mb-4 object-cover"
         />
         <h3 className="text-xl font-semibold text-center text-black">{name}</h3>
-        
       </div>
-      <div className="flex flex-col items-center mt-4">
-      <p className="text-gray-600 text-center font-medium mb-2">{role}</p>
-        <p className="text-gray-500 text-center mb-4">{description}</p>
+
+      {/* Content Section */}
+      <div className="flex flex-col items-center md:items-start text-center md:text-left mt-4 md:mt-0">
+        <p className="text-gray-600 font-medium mb-2">{role}</p>
+        <p className="text-gray-500 mb-4">{description}</p>
         <a
           href={linkedin}
           className="bg-black text-white p-2 rounded-full hover:bg-gray-800"
